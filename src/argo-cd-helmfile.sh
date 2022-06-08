dfd488cd97b15da3c144876e95834a6c403d176d
Tiberiu Patrascu <patrascu.tiberiu@gmail.com>
2022-06-08 11:06:53 +0300

    added HELMFILE_CACHE_CLEANUP option to clean up cache on init

--- src/argo-cd-helmfile.sh
+++ src/argo-cd-helmfile.sh
@@ -9,6 +9,7 @@
 # HELMFILE_HELMFILE - a complete helmfile.yaml (ignores standard helmfile.yaml and helmfile.d if present based on strategy)
 # HELMFILE_HELMFILE_STRATEGY - REPLACE or INCLUDE
 # HELMFILE_INIT_SCRIPT_FILE - path to script to execute during the init phase
+# HELMFILE_CACHE_CLEANUP - run helmfile cache cleanup on init
 # HELM_DATA_HOME - perform variable expansion
 
 # NOTE: only 1 -f value/file/dir is used by helmfile, while you can specific -f multiple times
@@ -197,6 +198,10 @@ case $phase in
     if [ ! -z "${HELMFILE_INIT_SCRIPT_FILE}" ]; then
       HELMFILE_INIT_SCRIPT_FILE=$(realpath "${HELMFILE_INIT_SCRIPT_FILE}")
       bash "${HELMFILE_INIT_SCRIPT_FILE}"
+    fi
+
+    if [[ "${HELMFILE_CACHE_CLEANUP}" ]]; then
+      ${helmfile} cache cleanup
     fi
 
     # https://github.com/roboll/helmfile/issues/1064
